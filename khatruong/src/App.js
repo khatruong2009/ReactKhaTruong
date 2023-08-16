2a78c60d7bdd5f252ad3bb1206aceaebcb77178d
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-08-15 19:26:59 -0700

    added support page

--- khatruong/src/App.js
+++ khatruong/src/App.js
@@ -5,18 +5,37 @@ import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+import Support from "./Components/Support";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/support" element={<Support/>} />
+        </Routes>
+        {/* <Header />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer /> */}
+      </div>
+    </BrowserRouter>
   );
 }
+
+const Home = () => (
+  <div>
+    <Header />
+    <AboutMe />
+    <Skills />
+    <Projects />
+    <Contact />
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ khatruong/src/Components/Support.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Support() {
+  return (
+    <div className="support" id="support">
+      <h2 className="support-header">App Support</h2>
+      
+      <p className="support-description">
+        If you're experiencing issues or have questions about our app, we're here to help! 
+        Please reach out to us directly for assistance.
+      </p>
+
+      <div className="contact-info">
+        <h3>Contact Information</h3>
+        <p>Email: khatruong2009@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Support;
